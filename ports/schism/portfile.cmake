3b258c52f7562271bc7a75d453ba16bd363bca7f
Angel Stojanov <stojanovangel@outlook.com>
2025-02-08 13:35:30 +0100

    setup

--- ports/schism/portfile.cmake
+++ ports/schism/portfile.cmake
@@ -1,9 +1,8 @@
 
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO https://github.com/42dotmk/schism
+    URL https://github.com/42dotmk/schism.git
     REF 9c2a35c5164970e5e7c6aaf38815b687b29b5045
-    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
@@ -12,6 +11,9 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "schism")
+
 vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
