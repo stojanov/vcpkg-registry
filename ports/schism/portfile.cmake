768bef84c7b8413ffef9b2e7dd5aa719dd41f1ee
Angel Stojanov <stojanovangel@outlook.com>
2025-02-08 14:28:58 +0100

    specify head ref

--- ports/schism/portfile.cmake
+++ ports/schism/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_git(
     URL https://github.com/42dotmk/schism.git
     REF 9c2a35c5164970e5e7c6aaf38815b687b29b5045
     SHA512 6b6d25034d379dca67e270dbe1229ab6b93e700442a25d6a7fec822d8d31943f8786e17cd62b58edd094376b9424a21b7019e0c21c29bc4deb540ea5c68bf6a5
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
