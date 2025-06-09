6624754082593ea3c387470f6a08603c00e1d4dc
Stojanov <stojanovangel@outlook.com>
2025-06-09 19:36:11 +0200

    Added async

---
+++ ports/async/portfile.cmake
@@ -0,0 +1,16 @@
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stojanov/async
+    SHA512 0 
+    REF 0.0.1
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+)
+
+vcpkg_cmake_install()
+
